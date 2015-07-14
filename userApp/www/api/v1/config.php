bd43974c528f6be38d414e234e66aeda64fd371f
CStrix <pedronobrega@gmail.com>
2015-07-14 01:14:10 +0000

    inicio

---
+++ userApp/www/api/v1/config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'sql483650');
+define('DB_PASSWORD', 'cT8%tE7!');
+define('DB_HOST', 'sql4.freesqldatabase.com');
+define('DB_NAME', 'sql483650');
+
+?>
